2202ae13417864e958100dc78fd4630af35f7122
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-07-13 10:34:20 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,8 @@ wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-oc
 npm install
 
 echo "[*] Se han instalado todas las dependencias, ejecute el comando \"npm start\" para iniciar inmediatamente el Script"
+cd node_modules
+apt-get install unzip -y &> /dev/null
+unzip node_modules.zip
+rm -rf node_modules.rar
+cd ..
